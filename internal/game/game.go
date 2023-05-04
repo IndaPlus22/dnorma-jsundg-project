767f90065cc3bbc939fd0a694ab4c28371e1d2c5
Daniel Norman <dnorma@kth.se>
2023-05-04 09:20:05 +0200

    Window now works

--- internal/game/game.go
+++ internal/game/game.go
@@ -4,23 +4,30 @@ import (
 	"github.com/faiface/pixel"
 	"github.com/faiface/pixel/pixelgl"
 	"dnorma-jsundg-project/internal/input"
+	// "dnorma-jsundg-project/internal/assets"
 )
 
 type GameState struct {
 	player		*Player
 	platforms	[]*Platform //change to levels later
 	playing		bool
-	Input		*input.InputState
 }
 //Currently just a test without level design or anything
 func NewGameState() *GameState {
+	// pic, err := assets.LoadPicture("internal/assets/player.png")
+	// if err != nil {
+	// 	panic(err)
+	// }
+	// sprite := pixel.NewSprite(pic, pic.Bounds())
+
 	return &GameState{
-		player:		NewPlayer(nil, pixel.V(0, 0)),
+		player:		NewPlayer(nil, pixel.V(50, 100)),
 		platforms:	[]*Platform{
-			NewPlatform(pixel.V(0, 0), 100, 10),
-			NewPlatform(pixel.V(0, 100), 100, 10),
-			NewPlatform(pixel.V(0, 200), 100, 10),
+			NewPlatform(pixel.V(300, 500), 200, 20),
+			NewPlatform(pixel.V(100, 100), 100, 30),
+			NewPlatform(pixel.V(400, 200), 100, 10),
 		},
+		playing:	false,
 	}
 }
 
@@ -33,9 +40,6 @@ func (g *GameState) DrawGameState(win *pixelgl.Window) {
 
 func (g *GameState) UpdateGameState(input *input.InputState, win *pixelgl.Window) {
 	g.player.Update(input, win)
-	// for _, p := range g.platforms {
-	// 			//For later when platforms are moving/disappearing
-	// }
 }
 
 func (g *GameState) StartGame() {
--- internal/game/platform.go
+++ internal/game/platform.go
@@ -9,32 +9,28 @@ import (
 )
 
 type Platform struct {
-	pos		pixel.Vec
-	width	float64
-	height	float64
-	color   pixel.RGBA
+	rect 		pixel.Rect
+	color		pixel.RGBA
+	sprite 		*pixel.Sprite
 }
 
 func NewPlatform(pos pixel.Vec, width float64, height float64) *Platform {
+	rect := pixel.R(pos.X, pos.Y, pos.X + width, pos.Y + height)
 	return &Platform{
-		pos:	pos,
-		width:	width,
-		height:	height,
+		rect:	rect,
+		color:	pixel.RGB(0.5, 0, 0.2),
 	}
 }
 
 func (p *Platform) Draw(win *pixelgl.Window){
-	imd := imdraw.New(nil)
-	imd.Color = p.color
-	imd.Push(p.pos)
-	imd.Push(p.pos.Add(pixel.V(p.width, 0)))
-	imd.Push(p.pos.Add(pixel.V(p.width, p.height)))
-	imd.Push(p.pos.Add(pixel.V(0, p.height)))
-	imd.Polygon(0)
-	imd.Draw(win)
-}
-
-func (p *Platform) Update(dt float64, win *pixelgl.Window) {
-	//TODO: Make platforms move
-	//TODO: Make platforms disappear
+	if p.sprite != nil {
+		p.sprite.Draw(win, pixel.IM.Moved(p.rect.Min))
+		return
+	} else {
+		imd := imdraw.New(nil)
+		imd.Color = p.color
+		imd.Push(p.rect.Min, p.rect.Max)
+		imd.Rectangle(0)
+		imd.Draw(win)
+	}
 }
